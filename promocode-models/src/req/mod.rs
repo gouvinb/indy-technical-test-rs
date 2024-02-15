dc7ceb4611c3dcdaf7b6c51c82c8373b56093994
gouvinb <brunogouvinhas@gmail.com>
2024-02-15 01:24:09 +0100

    style - Dev: format req/mod.rs and res/mod.rs

--- promocode-models/src/req/mod.rs
+++ promocode-models/src/req/mod.rs
@@ -1,3 +1,4 @@
+pub mod promocode_request;
 /// This module contains the [promocode_request_shadow] module.
 ///
 /// The [promocode_request_shadow] module provides helper functions and data
@@ -8,4 +9,3 @@
 ///
 /// - [serde-rs/serde#939-939514114](https://github.com/serde-rs/serde/issues/939#issuecomment-939514114)
 mod promocode_request_shadow;
-pub mod promocode_request;
--- promocode-models/src/res/mod.rs
+++ promocode-models/src/res/mod.rs
@@ -1,3 +1,4 @@
+pub mod promocode_accepted;
 /// This module contains the [promocode_accepted_shadow] module.
 ///
 /// The [promocode_accepted_shadow] module provides helper functions and data
@@ -8,8 +9,8 @@
 ///
 /// - [serde-rs/serde#939-939514114](https://github.com/serde-rs/serde/issues/939#issuecomment-939514114)
 mod promocode_accepted_shadow;
-pub mod promocode_accepted;
 
+pub mod promocode_denied;
 /// This module contains the [promocode_denied_shadow] module.
 ///
 /// The [promocode_denied_shadow] module provides helper functions and data
@@ -20,4 +21,3 @@ pub mod promocode_accepted;
 ///
 /// - [serde-rs/serde#939-939514114](https://github.com/serde-rs/serde/issues/939#issuecomment-939514114)
 mod promocode_denied_shadow;
-pub mod promocode_denied;
